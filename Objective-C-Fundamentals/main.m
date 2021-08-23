4b7fef1e58b25e2f0faaae97bc2670d98d6366b0
Marc <marcjacques@me.com>
2021-08-22 21:02:22 -0400

    Initial Commit

---
+++ Objective-C-Fundamentals/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Objective-C-Fundamentals
+//
+//  Created by Marc Jacques on 8/22/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
